7822b4025881730eacbe8a00b1f5948f7991ac26
software.saiyajin <gerald.adorza@navitaire.com>
2019-04-23 21:38:30 +0800

    Introduction

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ViewResult Random()
+        {
+            var movie = new Movie
+            {
+                Id = 1,
+                Name = "Shrek"
+            };
+            return View(movie);
+        }
+
+    }
+}
---
+++ Vidly/Models/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Movie()
+        {
+
+        }
+    }
+}
